70d2beb9997b4ceb5569bb6e72bc523a4c986370
butaoCodes <kelvin_freire@estudante.sesisenai.org.br>
2026-05-14 10:41:16 -0300

    Initial commit

---
+++ src/main/java/com/example/crudGamesProGit/CrudGamesProGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudGamesProGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudGamesProGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudGamesProGitApplication.class, args);
+	}
+
+}
